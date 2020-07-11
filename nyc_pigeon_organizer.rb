0e7a90cfce20cd35e42b34546ce50b528471333d
David Kirsch <davidjkirsch@gmail.com>
2020-07-11 18:34:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  data.map do |key, value|
+  
+  data.each do |key, value|
     value.each do |attributes, names|
       names.each do |name|
         if()
