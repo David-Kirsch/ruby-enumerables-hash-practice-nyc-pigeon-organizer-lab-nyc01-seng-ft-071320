ca6f13d782eb50fb58393240ff93993b68632eb5
David Kirsch <davidjkirsch@gmail.com>
2020-07-11 18:48:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
           pigeon_list[name] = {}
         end
         if(pigeon_list[name])
-          pigeon_list[name][key] = []
+          pigeon_list[name][key] = [attributes]
         end
       end
     end
