9f0ed7070003d1235f7056fe757a1adfc5145f10
David Kirsch <davidjkirsch@gmail.com>
2020-07-11 18:42:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,9 +6,9 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |attributes, names|
       names.each do |name|
-        if(pigeon_list[name] == false)
+        if(!pigeon_list[name])
              binding.pry
-          pigeon_list[name] = []
+          pigeon_list[name] = {}
           puts "hello"
         end
       end
